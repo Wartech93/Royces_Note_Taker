0b44c0a2f042389242f823ddc7ae95837ca26cb8
Sweaty Hog <Royceatkins93@gmail.com>
2024-05-01 19:12:34 -0400

    got server side functioning properly

--- Develop/routes/homeRoutes.js
+++ Develop/routes/homeRoutes.js
@@ -8,17 +8,17 @@ const {
 } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
-  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+notes.get('/notes', (req, res) => {
+  readFromFile('./Develop/db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // GET Route for a specific note
 notes.get('/:note_id', (req, res) => {
   const noteId = req.params.tip_id;
-  readFromFile('./db/notes.json')
+  readFromFile('./Develop/db/notes.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
-      const result = json.filter((note) => note.note_id === noteId);
+      const result = json.filter((note) => note.id === noteId);
       return result.length > 0
         ? res.json(result)
         : res.json('No note with that ID');
@@ -26,36 +26,36 @@ notes.get('/:note_id', (req, res) => {
 });
 
 // DELETE Route for a specific note
-notes.delete('/:note_id', (req, res) => {
-  const noteId = req.params.tip_id;
-  readFromFile('./db/notes.json')
+notes.delete('/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./Develop/db/notes.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all notes except the one with the ID provided in the URL
-      const result = json.filter((note) => note.note_id !== noteId);
+      const result = json.filter((note) => note.id !== noteId);
 
       // Save that array to the filesystem
-      writeToFile('./db/notes.json', result);
+      writeToFile('./Develop/db/notes.json', result);
 
       // Respond to the DELETE request
       res.json(`Item ${noteId} has been deleted 🗑️`);
     });
 });
 
-// POST Route for a new UX/UI note
-notes.post('/', (req, res) => {
+// POST Route for a new note
+notes.post('/notes', (req, res) => {
   console.log(req.body);
 
-  const { title, note } = req.body;
+  const { title, text } = req.body;
 
   if (req.body) {
     const newNote = {
       title,
-      note,
-      tip_id: uuidv4(),
+      text,
+      id: uuidv4(),
     };
 
-    readAndAppend(newNote, './db/notes.json');
+    readAndAppend(newNote, './Develop/db/notes.json');
     res.json(`Note added successfully`);
   } else {
     res.error('Error in adding note');
